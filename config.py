b956329af89c3d764ad5dde0ae6d77c732711ebc
Ezequiel Rivero <ezequielmrivero@gmail.com>
2024-12-22 12:03:44 +0100

    task1 q4 done

--- config.py
+++ config.py
@@ -3,23 +3,31 @@ import os
 # Project Root
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Paths
+# Data Paths
 DATA_PATH = os.path.join(ROOT, 'data')
 RAW_PATH = os.path.join(DATA_PATH, 'raw')
 PROCESSED_PATH = os.path.join(DATA_PATH, 'processed')
-MODELS_PATH = os.path.join(ROOT, 'models')
-PREDICTIONS_PATH = os.path.join(ROOT, 'predictions')
 
-# File Names
+# Raw Data Files
 PRODUCTS_DATA_PATH = os.path.join(RAW_PATH, 'products.pkl')
-USERS_DATA_PATH = os.path.join(PROCESSED_PATH, 'users.parquet')
 TRAIN_DATA_PATH = os.path.join(RAW_PATH, 'train.csv')
 TEST_DATA_PATH = os.path.join(RAW_PATH, 'test.csv')
+
+# Processed Data Files
+USERS_DATA_PATH = os.path.join(PROCESSED_PATH, 'users.parquet')
 TRAIN_PARQUET_PATH = os.path.join(PROCESSED_PATH, 'train.parquet')
 TEST_PARQUET_PATH = os.path.join(PROCESSED_PATH, 'test.parquet')
-SUBMISSION_1_PATH = os.path.join(PREDICTIONS_PATH, 'example_predictions_1.json')
-SUBMISSION_3_PATH = os.path.join(PREDICTIONS_PATH, 'example_predictions_3.json')
 PRODUCTS_PARQUET_PATH = os.path.join(PROCESSED_PATH, 'products.parquet')
+
+# Model Paths
+MODELS_PATH = os.path.join(ROOT, 'models')
+
+# Prediction Paths
+PREDICTIONS_PATH = os.path.join(ROOT, 'predictions')
+EX_SUBMISSION_1_PATH = os.path.join(PREDICTIONS_PATH, 'example_predictions_1.json')
+EX_SUBMISSION_3_PATH = os.path.join(PREDICTIONS_PATH, 'example_predictions_3.json')
+SUBMISSION_1_PATH = os.path.join(PREDICTIONS_PATH, 'predictions_1.json')
+SUBMISSION_3_PATH = os.path.join(PREDICTIONS_PATH, 'predictions_3.json')
 
 # API Configuration
 BASE_API_URL = "https://zara-boost-hackathon.nuwe.io"
